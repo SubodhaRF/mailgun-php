5e915d5f560f0df05e7824a21a73172c5d3843ae
subodhasahu <subodha.sahu@rapidfunnel.com>
2018-05-07 10:23:43 +0530

    Indents a flat JSON string to make it more human-readable

--- src/Mailgun/Messages/MessageBuilder.php
+++ src/Mailgun/Messages/MessageBuilder.php
@@ -310,7 +310,13 @@ class MessageBuilder
 
     public function addCustomData($customName, $data)
     {
-        $this->message['v:' . $customName] = json_encode($data);
+        $jsonData = json_encode($data);
+
+        if (strlen($jsonData) <= 998) {
+            $this->message['v:' . $customName] = $jsonData;
+        } else {
+            $this->message['v:' . $customName] = $this->indent($jsonData);
+        }
     }
 
     public function addCustomParameter($parameterName, $data)
@@ -339,5 +345,64 @@ class MessageBuilder
     public function getFiles()
     {
         return $this->files;
+    }
+
+    /**
+     * Indents a flat JSON string to make it more human-readable.
+     *
+     * @param string $json The original JSON string to process.
+     *
+     * @return string Indented version of the original JSON string.
+     */
+    public function indent($json)
+    {
+
+        $result = '';
+        $pos = 0;
+        $strLen = strlen($json);
+        $indentStr = ' ';
+        $newLine = "\n";
+        $prevChar = '';
+        $outOfQuotes = true;
+
+        for ($i = 0; $i <= $strLen; $i++) {
+
+            // Grab the next character in the string.
+            $char = substr($json, $i, 1);
+
+            // Are we inside a quoted string?
+            if ($char == '"' && $prevChar != '\\') {
+                $outOfQuotes = !$outOfQuotes;
+
+                // If this character is the end of an element,
+                // output a new line and indent the next line.
+            } else if (($char == '}' || $char == ']') && $outOfQuotes) {
+                $result .= $newLine;
+                $pos --;
+                for ($j = 0; $j < $pos; $j++) {
+                    $result .= $indentStr;
+                }
+            }
+
+            // Add the character to the result string.
+            $result .= $char;
+
+            // If the last character was the beginning of an element,
+            // output a new line and indent the next line.
+            if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+                $result .= $newLine;
+                if ($char == '{' || $char == '[') {
+                    $pos ++;
+                }
+
+                for ($j = 0; $j < $pos; $j++) {
+                    $result .= $indentStr;
+                }
+            }
+
+            $prevChar = $char;
+        }
+
+        return $result;
     }
 }
